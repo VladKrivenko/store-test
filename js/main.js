c1128f239e95dffd726f398f6aa92bea1c263599
VladKrivenko <nesslaen123@gmail.com>
2018-06-01 17:12:52 +0300

    sda

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ function init(){
 
 function goodsOut(data){
 	console.log(data);
+	var out='';
+	for (var key in data) {
+		out +='<div class="cart">';
+		out +='<p class="name">'+data[key].name+'</p>';
+		out += '<img src="img/'+data[key].img+'"alt="">';
+		out += '<p class="cost">'+data[key].cost+'</div>';
+		out +='<button class="add_to_cart">Buy</button>';
+		out +='</div>';
+	}
+	$('.store_items').html(out);
 }
 
-init();
+$(document).ready(function(){
+	init();
+});
