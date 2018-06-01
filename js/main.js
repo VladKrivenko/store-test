4445c2a96c177cec70364de7bb469f55bbc25760
VladKrivenko <nesslaen123@gmail.com>
2018-06-01 17:30:12 +0300

    sdas

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function goodsOut(data){
 		out +='<div class="cart">';
 		out +='<p class="name">'+data[key].name+'</p>';
 		out += '<img src="img/'+data[key].img+'"alt="">';
-		out += '<p class="cost">'+data[key].cost+'</div>';
+		out += '<p class="cost">'+data[key].cost+'</p>';
 		out +='<button class="add_to_cart">Buy</button>';
 		out +='</div>';
 	}
